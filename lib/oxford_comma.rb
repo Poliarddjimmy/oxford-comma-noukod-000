ad4f94a7871cbaf6f56611a7995fe396ed9f86c4
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-12 15:59:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,5 @@ def oxford_comma(array)
   return array.join if array.length <= 1
   return array.join(" and ") if array.length == 2
   return array.join(", ")
-  array[-1] = "and #{array[-1]}" if array.length > 2 
+  array[-1] = "and #{array[-1]}" if array.length >= 3
 end
